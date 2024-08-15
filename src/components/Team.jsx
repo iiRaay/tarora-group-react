90eb21b5391bb5437d23bae9fb25c72a7b053ce3
Raymond Duong <iiraay@hotmail.com>
2024-08-14 18:32:16 -0600

    added bios

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Team = (props) => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <p>Meet the passionate minds driving our success.</p>
         </div>
         <div id="row">
           {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                      {/* add description here d.description in data.json */}
+            ? props.data.map((d, i) => {
+                const toggleShowMore = () => {
+                  setShowMore(!showMore);
+                };
+
+                const bioPreview = d.bio.split(" ").slice(0, 20).join(" ");
+                const fullBio = d.bio;
+
+                return (
+                  <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                    <div className="thumbnail">
+                      <img src={d.img} alt="..." className="team-img" />
+                      <div className="caption">
+                        <h4>{d.name}</h4>
+                        <p>{d.job}</p>
+                        <p>
+                          {showMore ? fullBio : `${bioPreview}...`}
+                        </p>
+                        <p>
+                          <span
+                            onClick={toggleShowMore}
+                            style={{ color: "blue", cursor: "pointer" }}
+                          >
+                            {showMore ? " Show less" : " Show more"}
+                          </span></p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
+                );
+              })
             : "loading"}
         </div>
       </div>
