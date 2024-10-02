d566ff2dfdea6c37367d413cc8fb73db3f3c94d2
thovu-1 <148945562+thovu-1@users.noreply.github.com>
2024-10-01 18:55:43 -0600

    Rough draft of about us done
    Screenshot in jira ticket

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
-import { About } from "./components/about";
+import { About } from "./components/about_us/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
---
+++ src/components/about_us/about.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import './about.css';
+export const About = (props) => {
+  return (
+    <div id="about" >
+      <div className="container">
+        <div className="row">
+          <div className="header-container">
+            <div className="line-1"></div>
+                <h2 className="header-text">Welcome</h2>
+            <div className="line-2"></div>
+          </div>
+            <div className="about__welcome-text">
+                { props.data ? props.data.paragraph.map((p, i) => {
+                    return <p style={{fontSize:20}} key={i}>{p}</p>;
+                  }) : "...loading"
+                }
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="header-container">
+          <div className="line-1"></div>
+              <h2 className="header-text">Our Mission</h2>
+            <div className="line-2"></div>
+          </div>
+          <div className="about__our-mission-text-container">
+          { props.data ? props.data.ourmission.map((p, i) => {
+                    return <p style={{fontSize:20}} key={i}>{p}</p>;
+                  }) : "...loading"
+                }
+          </div>
+          
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="header-container">
+          <div className="line-1"></div>
+              <h2 className="header-text">Our Founder</h2>
+            <div className="line-2"></div>
+          </div>
+          <div className="col-xs-12 col-md-6 about__image-container">
+            {" "}
+            <img src="img/about.jpg" className="about__founder-image" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6 about__our-mission-text-container">
+          { props.data ? props.data.ourmission.map((p, i) => {
+                    return <p style={{fontSize:20}} key={i}>{p}</p>;
+                  }) : "...loading"
+                }
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
