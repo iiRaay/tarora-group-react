4c5babf2eff9ca36fb4b6c79a123cdaf83cc6a9e
Raymond Duong <iiraay@hotmail.com>
2024-10-03 22:52:06 -0600

    add url field to json for treatment page path

---
+++ src/components/treatments.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+
+
+export const Treatments = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+    </div>
+  );
+};
